ac8419e19a2b164dc64c05e9e94eaa00040a8e5a
Aliff Amir <aliff6406@gmail.com>
2025-06-14 18:41:36 +0100

    detect home end and del keys

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,18 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-namespace EditorKey
-{
 enum EditorKey
 {
     ARROW_UP = 1000,
     ARROW_RIGHT,
     ARROW_DOWN,
     ARROW_LEFT,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN,
 };
-} // namespace EditorKey
 
 struct editorConfig
 {
@@ -84,20 +86,60 @@ int editorReadKey()
             return '\x1b';
         if (read(STDIN_FILENO, &seq[1], 1) != 1)
             return '\x1b';
-
         if (seq[0] == '[')
         {
-            // alias arrow keys to wasd
+            if (seq[1] >= '0' && seq[1] <= '9')
+            {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '1':
+                        return HOME_KEY;
+                    case '3':
+                        return DEL_KEY;
+                    case '4':
+                        return END_KEY;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    case '7':
+                        return HOME_KEY;
+                    case '8':
+                        return END_KEY;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+                }
+            }
+        }
+        else if (seq[0] == 'O')
+        {
             switch (seq[1])
             {
-            case 'A':
-                return EditorKey::ARROW_UP;
-            case 'B':
-                return EditorKey::ARROW_DOWN;
-            case 'C':
-                return EditorKey::ARROW_RIGHT;
-            case 'D':
-                return EditorKey::ARROW_LEFT;
+            case 'H':
+                return HOME_KEY;
+            case 'F':
+                return END_KEY;
             }
         }
         return '\x1b';
@@ -213,25 +255,25 @@ void editorMoveCursor(int key)
 {
     switch (key)
     {
-    case EditorKey::ARROW_LEFT:
+    case ARROW_LEFT:
         if (E.cursorX > 0)
         {
             E.cursorX--;
         }
         break;
-    case EditorKey::ARROW_RIGHT:
+    case ARROW_RIGHT:
         if (E.cursorX < E.screencols)
         {
             E.cursorX++;
         }
         break;
-    case EditorKey::ARROW_UP:
+    case ARROW_UP:
         if (E.cursorY > 0)
         {
             E.cursorY--;
         }
         break;
-    case EditorKey::ARROW_DOWN:
+    case ARROW_DOWN:
         if (E.cursorY < E.screenrows)
         {
             E.cursorY++;
@@ -251,8 +293,25 @@ void editorProcessKeypress()
         exit(0);
         break;
 
-    case EditorKey::ARROW_UP:
-    case EditorKey::ARROW_LEFT:
+    case HOME_KEY:
+        E.cursorX = 0;
+        break;
+
+    case END_KEY:
+        E.cursorX = E.screencols - 1;
+        break;
+
+    case PAGE_UP:
+    case PAGE_DOWN: {
+        int times = E.screenrows;
+        while (times--)
+        {
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+        break;
+    }
+    case ARROW_UP:
+    case ARROW_LEFT:
     case EditorKey::ARROW_DOWN:
     case EditorKey::ARROW_RIGHT:
         editorMoveCursor(c);
