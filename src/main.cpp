c568da0e0c6ac509c7c9c6dea26eb3ab6dfd8d24
Aliff Amir <aliff6406@gmail.com>
2025-06-14 12:24:11 +0100

    use cursor position from bottom right as fallback to get terminal window size

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
-#include <errno.h>
+#include <cctype>
 #include <cstdio>
+#include <errno.h>
 #include <iostream>
+#include <string>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <cctype>
-#include <sys/ioctl.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -17,7 +18,7 @@ struct editorConfig
 
 editorConfig E;
 
-void die(const char *s)
+void die(const char* s)
 {
     perror(s);
     exit(1);
@@ -61,13 +62,42 @@ char editorReadKey()
     return c;
 }
 
-int getWindowSize(int &rows, int &cols)
+int getCursorPosition(int& rows, int& cols)
+{
+    std::string buf;
+    buf.reserve(32);
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    unsigned int i;
+    while (i < sizeof(buf) - 1)
+    {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        ++i;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    if (std::sscanf(&buf[2], "%d;%d", &rows, &cols) != 2)
+        return -1;
+
+    return 0;
+}
+
+int getWindowSize(int& rows, int& cols)
 {
     winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
     {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[99C\x1b[999B", 12) != 12)
+            return -1;
+        return getCursorPosition(rows, cols);
     }
     else
     {
@@ -81,7 +111,12 @@ void editorDrawRows()
 {
     for (int y{0}; y < E.screenrows; ++y)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenrows - 1)
+        {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
